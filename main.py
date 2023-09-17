cb2ad7d8fa438bd23e137480b3a25158a3b85243
Vinh <145184639+v-iinh@users.noreply.github.com>
2023-09-16 23:04:07 -0400

    Started the main logic for the audio transcription

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+#pip install SpeechRecognition 
+#conda istall pyaudios
 
+import speech_recognition as sr
+r=sr.Recognizer()
+
+with sr.AudioFile(eng_m7.wav) as source:
+    audio = r.listen(source)
+    try:
+        text = r.recognize_google(audio)
+        print("Working on ...")
+        print(text)
+    except:
+        print("sorry run again...")
