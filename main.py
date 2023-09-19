cf9efddda982eae818e1eca95499ba136e279088
Vinh <145184639+v-iinh@users.noreply.github.com>
2023-09-18 20:27:53 -0400

    Test update

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
-#pip install SpeechRecognition 
-#pip install Flask 
-
+from flask import Flask, request, render_template
+import os
 import speech_recognition as sr
-r=sr.Recognizer()
 
-with sr.AudioFile("user input") as source:
-    audio = r.listen(source)
-    try:
-        text = r.recognize_google(audio)
-        print("Working on ...")
-        print(text)
-    except:
-        print("Restart ...")
+app = (__name__)
+
+upload_folder = 'uploads'
+app.config['upload_folder', exist_ok=True]
+
+def transcribe_audio(audio_path):
+    r=sr.Recognizer()
+    with sr.AudioFile("user input") as source:
+        audio = r.listen(source)
+        try:
+            text = r.recognize_google(audio)
+            return text 
+        except sr.UnknownValueError: 
+            return "Could not understand audio."
+        except sr.RequestError as e: 
+            return "Could not request results: {0}.format(e)
+        
+@app.route('/', methods=['GET', 'POST'])
+def index():
