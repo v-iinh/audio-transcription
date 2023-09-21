f831e64a55a2051b0a77463c0e7f7fffd98b4f62
Vinh <145184639+v-iinh@users.noreply.github.com>
2023-09-21 16:00:40 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,43 +2,43 @@ from flask import Flask, request, render_template
 import os
 import speech_recognition as sr
 
-app = (__name__)
+app = Flask(__name__)
 
-upload_folder = 'uploads'
-app.config['upload_folder', exist_ok=True]
+# Corrected the configuration line
+app.config['UPLOAD_FOLDER'] = 'uploads'
 
 def transcribe_audio(audio_path):
-    r=sr.Recognizer()
-    with sr.AudioFile("user input") as source:
+    r = sr.Recognizer()
+    with sr.AudioFile(audio_path) as source:
         audio = r.listen(source)
         try:
             text = r.recognize_google(audio)
-            return text 
-        except sr.UnknownValueError: 
+            return text
+        except sr.UnknownValueError:
             return "Could not understand audio."
-        except sr.RequestError as e: 
-            return "Could not request results: {0}.format(e)
-        
+        except sr.RequestError as e:
+            return f"Could not request results: {e}"
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    if request.method == 'POST': 
-        if 'file' not in request.files: 
-            return "File Not Found" 
-        
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return "File Not Found"
+
         file = request.files['file']
 
         if file.filename == '':
             return "No selected file"
-        if file: 
-            audio_path = os.path.join(app.config['UPLOAD_Folder'], file.filename)
+        if file:
+            audio_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
             file.save(audio_path)
 
             transcription = transcribe_audio(audio_path)
             os.remove(audio_path)
 
             return "Transcription: " + transcription
-        
-        return render_template('index_html')
-    
-    if __name__ == '__main__': 
-        app.run(debug=True)
+
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
