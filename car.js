6f42f490939d227c2d2ac8b0a4df4d68bb8e9a8e
Sqibby <145184639+v-iinh@users.noreply.github.com>
2023-11-30 17:50:07 -0500

    updated car movement & sensors
    sensor detection isn't fully viable

--- car.js
+++ car.js
@@ -11,11 +11,14 @@ class Car{
         this.mapssed = 3;
         this.friction = 0.05; 
         this.angle=0; 
+
+        this.sensor=new Sensor(this); 
         this.controls=new Controls(); 
     }
 
-    update(){
+    update(roadBorders){
         this.#move(); 
+        this.sensor.update(roadBorders); 
     }
     
     #move(){
@@ -66,5 +69,7 @@ class Car{
         );
         ctx.fill();
         ctx.restore();
+
+        this.sensor.draw(ctx); 
     }
 }
