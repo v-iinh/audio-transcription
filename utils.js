a34d33b0299da663dd4162b9021a38f4c38eb10d
Sqibby <145184639+v-iinh@users.noreply.github.com>
2023-12-05 17:45:08 -0500

    updated sensors

--- utils.js
+++ utils.js
@@ -4,8 +4,8 @@ function lerp(A, B, t){
 
 function getIntersection(A,B,C,D){
     const tTop=(D.x-C.x)*(A.y-C.y)-(D.y-C.y)*(A.x-C.x); 
-    const uTop=(C.x-A.x)*(A.y-B.y)-(C.y-A.y)*(A.x-B.x); 
-    const bottom=(D.x-C.x)*(B.y-A.y)-(D.y-C.y)*(B.x-A.x); 
+    const uTop=(C.y-A.y)*(A.x-B.x)-(C.x-A.x)*(A.y-B.y); 
+    const bottom=(D.y-C.y)*(B.x-A.x)-(D.x-C.x)*(B.y-A.y); 
 
     if(bottom!=0){
         const t=tTop/bottom; 
@@ -20,4 +20,21 @@ function getIntersection(A,B,C,D){
     }
 
     return null; 
+}
+
+function polysIntersect(poly1, poly2){
+    for(let i=0; i<poly1.length;i++){
+        for(let j=0; j<poly2.length; j++){
+            const touch=getIntersection(
+                poly1[i],
+                poly1[(i+1)%poly1.length],
+                poly2[j],
+                poly2[(j+1)%poly2.length]
+            );
+            if(touch){
+                return true; 
+            }
+        }
+    }
+    return false; 
 }
